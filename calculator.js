02e8677317fd474e78a6ebb9ee1349b2a08b4340
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-05-03 23:34:25 +0100

    Add port

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var result = weight / (height * height);
+  res.send("Your BMI is" + result);
+});
+app.listen(process.env.PORT || 3000, function() {
+  console.log("server started on port 3000");
+
+});
